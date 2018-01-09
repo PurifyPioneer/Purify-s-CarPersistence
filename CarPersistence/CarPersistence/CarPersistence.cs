cfb121f2bee98b350c0c648ecad97b8e4888d964
PurifyPioneer <klook.christian@gmail.com>
2018-01-09 20:25:53 +0100

    Initial Commit.
    Basic Functionality is working. Data is not persistent over sessions. performance is bad(really).

---
+++ CarPersistence/CarPersistence/CarPersistence.cs
@@ -0,0 +1,293 @@
+﻿using GTA;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CarPersistence
+{
+    /* Goals:
+     *                             WORKING | WORKING | WORKING
+     *   1. Track driven vehicles (New | already known | respect limit)
+     *   
+     *                                   WORKING
+     *   2. Track saved vehicles (Transfer between driven and saved)
+     *
+     *   3. Blips (Different Color for driven/saved)
+     */
+
+    /* Notes on functionality:
+     * 
+     * 1.   Saved vehicles will be treated as driven vehicles. That means in a session you can take out
+     *      any of your saved cars drive around and it will only reset itself to the saved-position on a reload.
+     * 
+     * 2.   
+     */ 
+
+    public class CarPersistence : Script
+    {
+        // Easy availability
+        private Ped player;
+        // Limit how many vehicles we want to remember the player last entered
+        private int drivenVehiclesLimit = 3;
+
+        private LinkedList<Blip> blips = new LinkedList<Blip>();
+
+        // drivenVehicles contains all vehicles that are currently driven
+        private Dictionary<int, VehicleData> drivenVehicles = new Dictionary<int, VehicleData>();
+        // contains all vehicles that are saved but not currently driven
+        private Dictionary<int, VehicleData> savedVehicles = new Dictionary<int, VehicleData>();
+
+        public CarPersistence()
+        {
+            onLoad();
+
+            // delegates [tick comes from script and OnAbort forexample gets called...]
+            Tick += OnTick;
+            Aborted += OnAbort;
+
+            KeyDown += OnKeyDown;
+            KeyUp += OnKeyUp;
+
+            Interval = 50;
+        }
+
+        // Executes Logic that is needed on script-start
+        private void onLoad()
+        {
+            // Get player for easier access
+            player = Game.Player.Character;
+        }
+
+        // The actions that are perdormed on every tick
+        void OnTick(object sender, EventArgs e)
+        {
+            // If limit is exceeded oldest vehicles will be removed
+            cleanUpDrivenVehicles();
+            updateBlips();
+
+            // Logic to check if player is in a vehicle and if this vehicle is known to the system
+            if(player.IsInVehicle())
+            {
+                Vehicle v = player.CurrentVehicle;
+                int vHandle = v.Handle;
+
+                // Check if the vehicles is marked as driven
+                if(drivenVehicles.ContainsKey(vHandle))
+                {
+                    VehicleData tmp;
+                    drivenVehicles.TryGetValue(vHandle, out tmp);
+                    tmp.updateTimestamp();
+                }
+                // Check if the vehicle is saved
+                else if (savedVehicles.ContainsKey(vHandle))
+                {
+                    changeToDriven(v);
+                }
+                else
+                {
+                    // If vehicle is a new vehicle
+                    // TODO add vehicle to driven list
+                    changeToDriven(v);
+                }
+            }
+        }
+
+        // When the script is aborted/reloaded
+        void OnAbort(object sender, EventArgs e)
+        {
+            // MAKE SURE TO DELETE EVERY VEHICLE WE HAVE A HANDLE TO SO THERE APPEAR NO DUPLICATES ON RELOAD
+            clearBlips();
+        }
+
+        void OnKeyDown(object sender, KeyEventArgs e)
+        {
+        }
+        
+        void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.J)
+            {
+                toggleSaveState(player.CurrentVehicle);
+            }
+
+            if (e.KeyCode == Keys.K)
+            {
+                string vehicles = "";
+                foreach (KeyValuePair<int, VehicleData> entry in drivenVehicles)
+                {
+                    vehicles += entry.Value.name;
+                    vehicles += ";";
+                }
+                UI.Notify(vehicles);
+            }
+
+            if (e.KeyCode == Keys.U)
+            {
+                UI.Notify("" + player.CurrentVehicle.Handle);
+            }
+        }
+
+        /* Will look at all vehicles marked as currently driven
+         * and will remove the oldest vehicles if there are more
+         * vehicles than the set limit (First in first out)
+         * HINT: If a player stays in a vehicle or reenters one
+         * the timestamp will be reset. 
+        */
+        private void cleanUpDrivenVehicles()
+        {
+            // Cleans up as long as there are more driven vehicles than allowed
+            while (drivenVehicles.Count > drivenVehiclesLimit)
+            {
+                long oldestTime = getTimestamp();
+                int oldestKey =0;
+
+                // Finds the oldest vehicle
+                foreach (KeyValuePair<int, VehicleData> entry in drivenVehicles)
+                {
+                    if (entry.Value.timeStamp < oldestTime)
+                    {
+                        oldestTime = entry.Value.timeStamp;
+                        oldestKey = entry.Key;
+                    }
+                }
+
+                // Deletes the oldest vehicle or moves it to saved list
+                if(oldestKey != 0)
+                {
+                    changeToNoLongerDriven(oldestKey);
+                }
+            }
+        }
+
+        // Moves a vehicle to driven list
+        private void changeToDriven(Vehicle v)
+        {
+            // Handle saved vehicles
+            if (savedVehicles.ContainsKey(v.Handle))
+            {
+                // If vehicle is saved it is moved to driven list
+                VehicleData tmp;
+                savedVehicles.TryGetValue(v.Handle, out tmp);
+                tmp.timeStamp = getTimestamp();
+                drivenVehicles.Add(v.Handle, tmp);
+                savedVehicles.Remove(v.Handle);
+                v.IsPersistent = true;
+                UI.Notify("Moved saved vehicle [S -> D][" + v.DisplayName + "]");
+            }
+            // Handle new vehicles
+            else
+            {
+                // New vehicle is added to driven list
+                drivenVehicles.Add(v.Handle, new VehicleData(0, getTimestamp(), v.Model.Hash, v.DisplayName));
+                v.IsPersistent = true;
+                UI.Notify("Added unsaved Vehicle [" + v.DisplayName + "](" + drivenVehicles.Count + ")");
+            }
+        }
+
+        // Removes a vehicle from driven list
+        private void changeToNoLongerDriven(int handle)
+        {
+
+            VehicleData tmp;
+            drivenVehicles.TryGetValue(handle, out tmp);
+
+            if(!tmp.saved)
+            {
+                // Find vehicle in world
+                foreach (Vehicle ve in World.GetAllVehicles())
+                {
+                    if (ve.Handle == handle)
+                    {
+                        // Handle unsaved vehicles
+                        ve.IsPersistent = false;
+                        UI.Notify("Removed unsaved Vehicle [" + ve.DisplayName + "]");
+                        break;
+                    }
+                }
+                drivenVehicles.Remove(handle);
+            } else
+            {
+                // If the vehicle is saved the persistent status doesn't need to be adjusted
+                savedVehicles.Add(handle, tmp);
+                drivenVehicles.Remove(handle);
+                UI.Notify("Moved saved Vehicle [D -> S][" + tmp.name + "]");
+            }
+
+        }
+
+        // Marks the vehicle the player is in as saved
+        private void toggleSaveState(Vehicle v)
+        {
+            if (drivenVehicles.ContainsKey(v.Handle))
+            {
+                VehicleData tmp;
+                drivenVehicles.TryGetValue(v.Handle, out tmp);
+                tmp.saved = !tmp.saved;
+                if(tmp.saved)
+                {
+                    UI.Notify("Vehicle saved [" + v.DisplayName + "]");
+                } else
+                {
+                    UI.Notify("Vehicle unsaved [" + v.DisplayName + "]");
+                }
+            }
+        }
+
+        private void updateBlips()
+        {
+            clearBlips();
+            
+            foreach(KeyValuePair<int, VehicleData> entry in drivenVehicles)
+            {
+                Vehicle v = findVehicleByHandle(entry.Key);
+                if (v == null)
+                {
+                    continue;
+                }
+                Blip b = World.CreateBlip(v.Position);
+                b.Color = BlipColor.Red;
+                blips.AddLast(b);
+            }
+            foreach (KeyValuePair<int, VehicleData> entry in savedVehicles)
+            {
+                Vehicle v = findVehicleByHandle(entry.Key);
+                if (v == null)
+                {
+                    continue;
+                } 
+                Blip b = World.CreateBlip(v.Position);
+                b.Color = BlipColor.Green;
+                blips.AddLast(b);
+            }
+
+        }
+
+        private void clearBlips()
+        {
+            foreach (Blip b in blips)
+            {
+                b.Remove();
+            }
+        }
+
+        private Vehicle findVehicleByHandle(int handle)
+        {
+            foreach(Vehicle v in World.GetAllVehicles())
+            {
+                if(handle == v.Handle)
+                {
+                    return v;
+                }
+            }
+            return null;
+        }
+
+        public static long getTimestamp()
+        {
+            var timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (long)timeSpan.TotalSeconds;
+        }
+    }
+
+ 
+}
---
+++ CarPersistence/CarPersistence/VehicleData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPersistence
+{
+    class VehicleData
+    {
+        private int ID;
+        public bool saved { get; set; }
+        public long timeStamp { get; set; }
+        private int modelHash;
+        public string name { get; set; }
+
+        public VehicleData(int ID, long timeStamp, int modelHash, string name)
+        {
+            this.ID = ID;
+            this.timeStamp = timeStamp;
+            this.modelHash = modelHash;
+            this.name = name;
+        }
+
+        public void updateTimestamp()
+        {
+            this.timeStamp = CarPersistence.getTimestamp();
+        }
+    }
+}
